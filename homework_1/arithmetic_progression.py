0ef6ba3596ba221946facd7c4b1229b82cf43fc8
Liana Beglaryan <li.beglaryan@mail.ru>
2020-11-26 18:15:52 +0400

    First homework

---
+++ homework_1/arithmetic_progression.py
@@ -0,0 +1,6 @@
+num1 = int(input("first number is: "))
+num2 = int(input("second number is: "))
+dif = num2-num1
+n = int(input("which number you want: "))
+n_number = num1 + dif*(n-1)
+print(n_number)
