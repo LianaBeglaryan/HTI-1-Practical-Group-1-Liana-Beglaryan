b2168d8d46a5769438d0ceb6439dd578c3bc5022
LianaBeglaryan <74502069+LianaBeglaryan@users.noreply.github.com>
2020-12-03 20:20:48 +0400

    Delete classporl.py

--- homework_1/classporl.py
+++
@@ -1,9 +0,0 @@
-n=[int(n) for n in input("tpel tiv: ").split()]
-
-for i in range(0,len(n)-1):
-    if n[i]>n[i+1]:
-        n[i],n[i+1]=n[i+1],n[i]
-        print(n)
-
-    elif n[i]<n[i+1]:
-        print("everything id okay")    
