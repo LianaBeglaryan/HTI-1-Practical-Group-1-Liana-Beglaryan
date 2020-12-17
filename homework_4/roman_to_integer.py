4283e5fff4cb2309cb3810c9e0bf9a63d1f07222
LianaBeglaryan <74502069+LianaBeglaryan@users.noreply.github.com>
2020-12-18 00:17:43 +0400

    Update roman_to_integer.py

--- homework_4/roman_to_integer.py
+++ homework_4/roman_to_integer.py
@@ -1,23 +1,14 @@
-def romanToInt(s) -> int:
-        romanHash = {
-            'I':1,
-            'V':5,
-            'X':10,
-            'L':50,
-            'C':100,
-            'D':500,
-            'M':1000
-        }
+def roman_to_int(s):
+        roman = {'I':1 ,'V':5 ,'X':10,'L':50,'C':100,'D':500,'M':1000}
         value = 0
-        previous = number = romanHash[s[0]]
+        previous = number = roman[s[0]]
         for r in s[1:]:
-            value = romanHash[r]
+            value = roman[r]
             if value > previous:
-                number = number + value - 2 * previous
-            else:
-                number = number + value
+                number = number + value - 2 * previous        
+            number = number + value
             previous = value
         return number
-
-s=input("tpel taretiv: ")        
-romanToInt(s)
+       
+s = input("enter a date: ")        
+print(roman_to_int(s))
